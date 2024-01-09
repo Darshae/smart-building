3c99635d1b0fce00161d2017fe08cfc8cca6226e
Darshae <dallasvalderama@gmail.com>
2024-01-09 22:53:01 +0800

    first commit

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="sideBar">
+    <ul>
+        <li>Overview</li>
+        <li>Buildings</li>
+        <li>Heating Schedule</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
